c8d2e5fb28ac3868e87b6e0e06efff696a03a1cf
DakshGoswami5 <dakshgoswami2@gmail.com>
2025-11-04 05:45:50 +0530

    storage.servoce.js file created

--- src/controllers/post.controller.js
+++ src/controllers/post.controller.js
@@ -1,17 +1,40 @@
 const postModel = require("../models/post.model");
 const generateCaption = require("../service/ai.service")
+const uploadFile = require("../service/storage.service")
+const { v4: uuid4 } = require("uuid")
 
 async function createPostController(req,res) {
-    const file =req.file;
-    console.log("File received:", file);
+    try {
+        const file = req.file;
+        console.log("File received:", file);
 
-    const base64Image = new Buffer.from(file.buffer).toString("base64");
+        // Convert to base64 for AI caption generation
+        const base64Image = Buffer.from(file.buffer).toString("base64");
 
-    const caption = await generateCaption(base64Image);
+        // Generate caption
+        const caption = await generateCaption(base64Image);
 
-    res.json({
-        caption
-    })
+        // Upload to ImageKit (it will convert buffer to base64 internally)
+        const result = await uploadFile(file.buffer, `${uuid4()}`);
+
+        // Create post in database
+        const post = await postModel.create({
+            caption: caption,
+            image: result.url,
+            user: req.user._id
+        });
+
+        res.status(201).json({
+            message: "Post Created successfully",
+            post
+        });
+    } catch (error) {
+        console.error("Error creating post:", error);
+        res.status(500).json({
+            message: "Error creating post",
+            error: error.message
+        });
+    }
 } 
 
 module.exports = {
