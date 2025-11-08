238d1e5dd11c73242c6ee6ab1e09cb3760e787d9
DakshGoswami5 <dakshgoswami2@gmail.com>
2025-11-08 16:51:06 +0530

    backend updated

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -73,6 +73,8 @@ async function loginController(req, res) {
   httpOnly: true,
   secure: process.env.NODE_ENV === "production" ? true : false,
   sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
+  maxAge: 24 * 60 * 60 * 1000, // 1 day
+  path: '/' 
 });
 
     res.status(200).json({
